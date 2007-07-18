0790814cb256a9b88cd0c53df98835038d51e143
jayshree_tp <jayshree_tp@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2007-07-18 11:44:28 +0000

    Fixed compilation errors while integrating the API. Working on the view part

--- hpiview.py
+++ hpiview.py
@@ -1,14 +1,15 @@
- #!/usr/bin/env python
+#!/usr/bin/env python
 
 import wx
-import hpiview_window
-import hpiview_callbacks
+from hpiview_window import *
+from hpiview_callbacks import *
 
 
 if __name__ == "__main__":
     app = wx.PySimpleApp(0)
     wx.InitAllImageHandlers()
-    frame_1 = hpiview_window.Hpiview_Window(None, -1, "")
+    frame_1 = Hpiview_Window(None, -1, "")
+    callback = Hpiview_Callbacks(frame_1)
     app.SetTopWindow(frame_1)
     frame_1.Show()
     app.MainLoop()
