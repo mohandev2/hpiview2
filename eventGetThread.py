3ef424c24147b7cc428e2d47de436925b41a1aa6
jayshree_tp <jayshree_tp@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2007-12-05 14:45:33 +0000

    added a custom user event and then using the eventget method to get the events successfully. the eventget runs in a different thread and catches the event and displays it in the events window when the events are subscribed.

--- eventGetThread.py
+++ eventGetThread.py
@@ -19,23 +19,23 @@ class EventGetThread(Thread):
 
     listctrl = None
 
+
     def __init__(self,lstctrl,sessionid):
         global listctrl,sid
         Thread.__init__(self)
         listctrl = lstctrl
         sid=sessionid
+        self.AddEvent()
 
     def run(self):
-	print "Listening for Events .... "
-	thread.start_new_thread (self.CatchEvent,())
-
-
-    def CatchEvent(self):
         global res
         global rdr
         global sid
         global timeout
         global event
+        global listctrl
+
+	print "Listening for Events .... "
         self.rdr.RdrType = SAHPI_NO_RECORD
         error, qstatus = saHpiEventGet(sid, self.timeout, self.event, self.rdr, self.res)
         if error != SA_OK:
@@ -48,11 +48,54 @@ class EventGetThread(Thread):
                     print 'ERROR: Time, %u seconds, expired waiting for event' % options.timeout
         else:
             if self.rdr.RdrType == SAHPI_NO_RECORD:
-                oh_print_event(self.event, None, 4)
-            else:
-##                    oh_print_event(event, rdr.Entity, 4)
+                tbuff = oh_big_textbuffer()
                 b=SaHpiTextBufferT()
                 oh_decode_time(self.event.Timestamp, b)
-                oh_init_bigtext(tbuff)
                 oh_decode_entitypath(self.rdr.Entity, tbuff)
-                self.listctrl.append([b.Data,tbuff.Data,oh_lookup_severity(self.event.Severity),oh_lookup_eventtype(self.event.EventType),str(self.event.EventDataUnion.DomainEvent.DomainId)])
+                listctrl.Append([b.Data,tbuff.Data,oh_lookup_severity(self.event.Severity),oh_lookup_eventtype(self.event.EventType),str(self.event.EventDataUnion.DomainEvent.DomainId)])
+##                oh_print_event(self.event, None, 4)
+            else:
+                tbuff = oh_big_textbuffer()
+                b=SaHpiTextBufferT()
+                oh_decode_time(self.event.Timestamp, b)
+                oh_decode_entitypath(self.rdr.Entity, tbuff)
+                listctrl.Append([b.Data,tbuff.Data,oh_lookup_severity(self.event.Severity),oh_lookup_eventtype(self.event.EventType),str(self.event.EventDataUnion.DomainEvent.DomainId)])
+
+    def AddEvent(self):
+        global sid
+        # the event initialized
+        event =  SaHpiEventT()
+
+        # initialize the mandatory attributes
+        event.EventType = SAHPI_ET_USER
+        event.Source = SAHPI_UNSPECIFIED_RESOURCE_ID
+
+        # initialize the attreibutes for the specific kind of event that is to be triggerred.
+
+        sensorevt = SaHpiSensorEventT()
+        sensorchangeevt = SaHpiSensorEnableChangeEventT()
+        userevt = SaHpiUserEventT()
+
+        sensorevt.SensorNum = 222
+        sensorevt.SensorType = SAHPI_TEMPERATURE
+        sensorevt.EventCategory = SAHPI_EC_THRESHOLD
+        sensorevt.Assertion = True
+
+        sensorchangeevt.SensorNum = 222
+        sensorchangeevt.SensorEnable = True
+
+        textbuffer = SaHpiTextBufferT()
+        oh_append_textbuffer(textbuffer," Adding Jaysjree s own event to have look at how the events work. ofcourse written by prashanth ")
+
+        userevt.UserEventData = textbuffer
+
+
+        ##event.EventDataUnion.SensorEvent = sensorevt
+        ##event.EventDataUnion.SensorEnableChangeEvent = sensorchangeevt
+        event.EventDataUnion.UserEvent = userevt
+
+
+        error = saHpiEventAdd(sid, event)
+        error = saHpiEventLogEntryAdd (sid , SAHPI_UNSPECIFIED_RESOURCE_ID , event)
+        
+        print "Added the Custom User Event onto the Events Stack"
